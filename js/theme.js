2eac1eb15269584b9f4f7eb0224b004301ce9b6c
Mrityunjay Gupta <156502183+MrityunjayGupta1@users.noreply.github.com>
2026-01-16 21:20:12 +0530

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,1 @@
-const toggleBtn = document.getElementById("themeToggle");
 
-if (localStorage.getItem("theme") === "dark") {
-  document.body.classList.add("dark-mode");
-  toggleBtn.textContent = "☀️";
-}
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-
-  if (document.body.classList.contains("dark-mode")) {
-    localStorage.setItem("theme", "dark");
-    toggleBtn.textContent = "☀️";
-  } else {
-    localStorage.setItem("theme", "light");
-    toggleBtn.textContent = "🌙";
-  }
-});
